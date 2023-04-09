e5a4b97c57433c2efa8b1b2b1a0cf5bb3f857e0c
meelis.utt <meelis.utt@gmail.com>
2023-04-10 00:39:58 +0200

    tokenize strings and numbers

--- token.c
+++ token.c
@@ -1,10 +1,14 @@
 # include <stdio.h>
 # include <stdint.h>
+# include <stdlib.h>
+# include <string.h>
 
-#define MAX 1024*10
+#define MAX 1024*10 // TODO: make more robust
+#define MAX_STR 100 // TODO: make more robust
+
 
 typedef enum {
-	CHAR = 0,
+	STRING = 0,
 	NUMBER = 1,
 	TILDE = '~',
 	TICK = '`',
@@ -48,37 +52,71 @@ struct MyFloat{
 
 typedef struct {
 	TOKEN token;
-	char value;
+	char val[MAX_STR];
 } MyToken;
 
+void t2s(MyToken t) {
+	switch (t.token) {
+		case STRING:
+			printf("STRING(%s)\n", t.val);
+			break;
+		case NUMBER:
+			printf("NUMBER(%s)\n",t.val);
+			break;
+		case TAB:
+			printf("SYMOBL(\\t)\n");
+			break;
+		case NEWLINE:
+			printf("SYMBOL(\\n)\n");
+			break;
+		case SPACE:
+			printf("SYMBOL(' ')\n");
+			break;
+		default:
+			printf("SYMBOL(%s)\n", t.val);
+	}
+}
 
 int main(int argc, char** argv) {
-	// printf(MyToken(At,"@"));
-	char* test = "see lause < on test> ?";
-	printf("Hello world %d %d\n", NUMBER, PIPE);
-	printf("%s %d\n", test, (TOKEN)'|');
 	int c;
-
 	MyToken tokens[MAX];
-	char* val;
+	char val[MAX_STR];
 	int i = 0;
+	int k = 0;
 	while ((c=getchar()) != EOF) {
-		MyToken token;
-		token.value = c;
+	//while ( (c = *test++) != '\0') {
+		MyToken token = {};
 		if (c >= 'a' && c <= 'z') {
-			token.token = CHAR;
+			token.token = STRING;
+			val[k] = (char)c;
+			k++;
 		} else if ( c >= 'A' && c <= 'Z'){
-			token.token = CHAR;
+			token.token = STRING;
+			val[k] = (char)c;
+			k++;
+		} else if ( c >= '0' && c <= '9') {
+			token.token = NUMBER;
+			val[k] = (char)c;
+			k++;
 		} else {
+			// NOTE: when string/number ends, store the token and prep for new symbol
+			// TODO: support floats, scientific notation, numbers in strings etc
+			if (k > 0) {
+				strcpy(token.val,val);
+				k=0;
+				memset(val, 0, MAX_STR);
+				tokens[i] = token;
+				i++;
+				MyToken token = {};
+			}
 			token.token = c;
+			strcpy(token.val, (char*)&c);
+			tokens[i] = token;
+			i++;
 		}
-		tokens[i] = token;
-		i++;
 	}
 	
 	for (int j=0; j<i;++j) {
-		// printf("TOKEN: %d %d\n", tokens[j].token, tokens[j].value);
-		printf("%c", (char)tokens[j].value);
+		t2s(tokens[j]);
 	}
-
 }
